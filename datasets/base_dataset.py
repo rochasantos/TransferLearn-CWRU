a432932574f69c5c06c74d9ef49e7cf0a85023fe
Adriano Rocha <adriano_angelo@live.com>
2024-10-05 10:05:11 -0300

    refactore

--- datasets/base_dataset.py
+++ datasets/base_dataset.py
@@ -1,9 +1,12 @@
 import os
+import numpy as np
+import re
+import scipy.io
 from utils.download_extract import download_file
 
 class BaseDataset:
     
-    def __init__(self, rawfilesdir, url):
+    def __init__(self, rawfilesdir, spectdir, sample_rate, url):
         """
         Base class for all datasets. 
         Defines attributes and a download function to be inherited by specific datasets.
@@ -12,12 +15,16 @@ class BaseDataset:
         - rawfilesdir (str): The directory where raw files will be stored.
         - url (str): The base URL for downloading the dataset files.
         """
-        self.rawfilesdir = rawfilesdir  # Directory to store the raw files
-        self.url = url  # Base URL for downloading the files
+        self._rawfilesdir = rawfilesdir  # Directory to store the raw files
+        self._spectdir = spectdir  # Directory to store the raw files
+        self._sample_rate = sample_rate  # Directory to store the raw files
+        self._url = url  # Base URL for downloading the files
+        self._data = []
+        self._label = []
 
         # Check if the raw files directory exists, if not, create it
-        if not os.path.exists(self.rawfilesdir):
-            os.makedirs(self.rawfilesdir)
+        if not os.path.exists(self._rawfilesdir):
+            os.makedirs(self._rawfilesdir)
 
     
     def download(self):
@@ -33,4 +40,23 @@ class BaseDataset:
         for filename, url_suffix in bearings:
             download_file(url, dirname, url_suffix, filename)
         print("Download finished.")
-  
+
+    @property
+    def data(self):
+        return self._data
+    
+    @property
+    def label(self):
+        return self._label
+    
+    @property
+    def rawfilesdir(self):
+        return self._rawfilesdir
+    
+    @property
+    def spectdir(self):
+        return self._spectdir
+    
+    @property
+    def sample_rate(self):
+        return self._sample_rate
