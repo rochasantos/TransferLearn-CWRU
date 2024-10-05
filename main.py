8a39dc46f5d43cf0112b2c5d6dbd9721bfd53371
Adriano Rocha <adriano_angelo@live.com>
2024-10-05 14:52:21 -0300

    remove unnecessary lines of code.

--- main.py
+++ main.py
@@ -3,5 +3,8 @@ from datasets.uored import UORED
 from datasets.hust import Hust
 from datasets.paderborn import Paderborn
 
+def main():
+    pass
+
 if __name__ == '__main__':
-    Paderborn().download()
+    main()
