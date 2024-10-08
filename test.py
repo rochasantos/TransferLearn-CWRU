958eab6361768a7de9533bdef9b79edc9240d629
Adriano Rocha <adriano_angelo@live.com>
2024-10-08 14:30:33 -0300

    clean: remove unnecessary lines code.

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import numpy as np
+import os
 from datasets.cwru import CWRU
 from datasets.paderborn import Paderborn
 from datasets.hust import Hust
@@ -7,16 +9,18 @@ from src.feature_engineering.create_spectrogram import generate_spectrogram
 class DatasetModelFactory:
     @staticmethod
     def create_dataset(dataset_name):
-        if dataset_name == 'paderborn':
-            return Paderborn(debug=True)
-        elif dataset_name == "cwru":
-            return CWRU(debug=True)
-        elif dataset_name == "hust":
-            return Hust(debug=True)
-        elif dataset_name == "uored":
-            return UORED(debug=True)
-        else:
+        try:
+            dataset = eval(f'{dataset_name}(debug=True)')
+        except:
             raise ValueError("Unknown dataset.")
+        
+        dataset._rawfilesdir = f'data/raw_test/{dataset}'
+        dataset._spectdir = f'data/processed_test/{dataset}'
+
+        if not os.path.exists(dataset.spectdir):
+            os.makedirs(dataset.spectdir, exist_ok=True)
+
+        return dataset
 
 fct = DatasetModelFactory()
 
@@ -25,32 +29,28 @@ Download and Extract files
 """
 def test_download(dataset_name):
     dataset = fct.create_dataset(dataset_name)
-    dataset._rawfilesdir = f'data/raw_test/{dataset}'
     dataset.download()
 
 """
 Load Signal
 """
-def test_load_signal_with_acquisition_maxsize_none(dataset_name):
+def test_load_signal(dataset_name):
     dataset = fct.create_dataset(dataset_name)    
     dataset.load_signal()
-    print('** test_load_signal_with_acquisition_maxsize_none')
-    print(f'- data.shape: {dataset.data.shape}, label.shape: {dataset.label.shape}\n')
-
-def test_load_signal_with_acquisition_maxsize_64000(dataset_name):
-    dataset = fct.create_dataset(dataset_name)    
-    dataset.load_signal(acquisition_maxsize=64000)
-    print('** test_load_signal_with_acquisition_maxsize_64000')
-    print(f'- data.shape: {dataset.data.shape}, label.shape: {dataset.label.shape}\n')
+    print('** testing load_signal function.')
+    print(f'- data.shape: {dataset.data.shape}, label.shape: {dataset.label.shape}')
+    print(f'- labels: {np.unique(dataset.label)}\n')
 
 def test_load_signal_with_filter_file(dataset_name):
     if dataset_name == 'paderborn':
         regex = r'.*N09_M07_F10_K001_[12]\.mat$'
     elif dataset_name == 'cwru':
         regex = r'B\.007\.DE_[01]&12000.mat'
+    else:
+        regex = f'.*\.mat'
     dataset = fct.create_dataset(dataset_name) 
     dataset.load_signal(regex_filter=regex)
-    print('** test_load_signal_with_acquisition_maxsize_64000_x_3_with_filter')
+    print('** test_load_signal_with_filter')
     print(f'- data.shape: {dataset.data.shape}, label.shape: {dataset.label.shape}\n')
 
 """
@@ -64,18 +64,16 @@ def test_create_spectrograms(dataset_name):
 
 
 def test():
-    dataset_name = 'uored'
+    dataset_name = 'UORED'
     # download
     test_download(dataset_name)
-"""
+    
     # load signal
-    test_load_signal_with_acquisition_maxsize_none(dataset_name)
-    test_load_signal_with_acquisition_maxsize_64000(dataset_name)
+    test_load_signal(dataset_name)
     test_load_signal_with_filter_file(dataset_name)
 
     # generate spectrograms
     test_create_spectrograms(dataset_name)
-"""
 
 if __name__ == '__main__':
     test()
