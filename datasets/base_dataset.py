8349f159b0836d9468f9e8d7f81f00ae437cf5c8
Adriano Rocha <adriano_angelo@live.com>
2024-10-18 20:51:24 -0300

    remove attributes unnecessary

--- datasets/base_dataset.py
+++ datasets/base_dataset.py
@@ -1,13 +1,16 @@
 import os
 import numpy as np
 import re
+import pandas as pd
+import csv
 from utils.download_extract import download_file
+from src.data_processing.annotation_file import AnnotationFileHandler
 
 from abc import ABC, abstractmethod
 
 class BaseDataset(ABC):
     
-    def __init__(self, rawfilesdir, spectdir, sample_rate, url, debug, config='all'):
+    def __init__(self, rawfilesdir, url):
         """
         Base class for all dataset models. 
         Defines the attributes and the download and load_signal functions, 
@@ -23,18 +26,16 @@ class BaseDataset(ABC):
             download(): Downloads .mat from the dataset website URL.
         """
         self._rawfilesdir = rawfilesdir  # Directory to store the raw files
-        self._spectdir = spectdir  # Directory to store the spectrogram files
-        self._sample_rate = sample_rate  # Sampling rate of the data acquisition
         self._url = url  # Base URL for downloading the files
         self._data = [] # List to store the extracted data.
         self._label = []  # List to store the corresponding labels for the data.
-        self.debug = debug  # Flag to anable or disable debug mode for testing.
         self.acquisition_maxsize = None  # Maximum size for data acquisition.
+        self._annotation_file=AnnotationFileHandler().filter_data(dataset_name=self.__class__.__name__)
+
 
         if not os.path.exists(self._rawfilesdir):
-            os.makedirs(self._rawfilesdir)
+            os.makedirs(self._rawfilesdir)   
 
-    
     def download(self):
         """ Download files from datasets website.
         """
@@ -43,11 +44,15 @@ class BaseDataset(ABC):
         if not os.path.isdir(dirname):
             os.mkdir(dirname)
         print(f"Stating download of {self} dataset.")
-        bearings = self.list_of_bearings()
-        for filename, url_suffix in bearings:
+        filename_list = [info["filename"]+'.mat' for info in self.annotation_file]
+        for filename in filename_list:
             if not os.path.exists(os.path.join(dirname, filename)):
-                download_file(url, url_suffix, dirname, filename)
+                download_file(url, filename, dirname, filename)
         print("Download finished.")
+
+    def load_signal_by_path(self, filepath):
+        signal, label = self._extract_data(filepath)
+        return signal, label
     
     def load_signal(self, regex_filter=r'.*\.mat$'):
         """ Load vibration signal data from .mat files, filtered by a regex. 
@@ -63,10 +68,8 @@ class BaseDataset(ABC):
         for root, dirs, files in os.walk(self.rawfilesdir):
             for file in files:
                 filepath = os.path.join(root, file)
-
                 if not regex.search(file):
                     continue
-
                 data, label = self._extract_data(filepath)
                 signal.append(data)
                 labels.append(label)
@@ -84,11 +87,7 @@ class BaseDataset(ABC):
         Returns:
             tuple: A tuple containing (data, label), where 'data' is the extracted dataset and 'label' is the corresponding label.
         """
-        pass
-
-    @property
-    def url(self):
-        return self._url
+        pass  
 
     @property
     def data(self):
@@ -101,11 +100,7 @@ class BaseDataset(ABC):
     @property
     def rawfilesdir(self):
         return self._rawfilesdir
-    
+        
     @property
-    def spectdir(self):
-        return self._spectdir
-    
-    @property
-    def sample_rate(self):
-        return self._sample_rate
+    def annotation_file(self):
+        return self._annotation_file
