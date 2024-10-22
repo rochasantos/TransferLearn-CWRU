e83945976551dc95564b1007d63a3798395ae72e
Adriano Rocha <adriano_angelo@live.com>
2024-10-22 08:38:36 -0300

    changing the end of the file name to # instead of _ to avoid conflict with other file names.

--- src/feature_engineering/spectrogram/generate_spectrogram.py
+++ src/feature_engineering/spectrogram/generate_spectrogram.py
@@ -16,7 +16,7 @@ def generate_spectrogram(data_raw, label, basename, window_size, num_segments,
     
     for i in range(0, window_size * n_max_segments, window_size):
         # Save the spectrogram image to the specified output directory
-        output_file = os.path.join(output_dir, label, basename+'_{}.png'.format(int((i+1)/window_size)))
+        output_file = os.path.join(output_dir, label, basename+'#{}.png'.format(int((i+1)/window_size)))
         
         if os.path.exists(output_file):
             continue
