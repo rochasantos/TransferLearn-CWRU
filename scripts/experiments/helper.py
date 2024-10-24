6e9941fad7a752a5537e238e126eaa571a9f6c7c
Adriano Rocha <adriano_angelo@live.com>
2024-10-24 15:17:33 -0300

    creating a grouper to create groups to stratifiedGroupKFold.

---
+++ scripts/experiments/helper.py
@@ -0,0 +1,45 @@
+import os
+import torch
+import torch.nn as nn
+from src.data_processing.annotation_file import AnnotationFileHandler
+
+annot = AnnotationFileHandler()
+
+def get_counter(dataset, feature_mitigation):
+    counter = {}
+    for i in range(len(dataset)):
+        basename = os.path.basename(dataset.samples[i][0])
+        bearing_info = annot.filter_data(filename=basename.split('#')[0])[0]
+        feature_value = bearing_info[feature_mitigation]
+
+        if feature_value not in counter:
+            counter[feature_value]=0
+        counter[feature_value]+=1
+
+    return counter
+
+def grouper(dataset, feature_mitigation):
+    groups=[]
+    hash = dict()
+    counter = get_counter(dataset, feature_mitigation)
+    c=0
+    for i in range(len(dataset)):
+        path = dataset.samples[i][0]
+        basename = os.path.basename(path)
+        bearing_info = annot.filter_data(filename=basename.split('#')[0])[0]
+        feature_value = bearing_info[feature_mitigation]
+        # Distribution of reference bearing values.
+        if bearing_info["extent_damage"]=='000':
+            groups.append(c)
+            c = c + 1 if c < len(counter)-2 else 0
+        else:
+            if feature_value not in hash:
+                hash[feature_value] = len(hash)
+            groups.append(hash[feature_value])
+
+
+    print('Groups:', set(groups))
+    print('Couter:', counter)
+    print('Hash:', hash)
+
+    return groups
