ac132e6b19706c5f8ad5eb6f9b9af08d48c69129
Adriano Rocha <adriano_angelo@live.com>
2024-11-05 00:38:19 -0300

    add NormalizationStrategy to __init__

--- src/preprocessing/__init__.py
+++ src/preprocessing/__init__.py
@@ -3,3 +3,4 @@ from .resample import ResamplingStrategy
 from .zero_mean import ZeroMeanStrategy
 from .outlier_removal import OutlierRemovalStrategy
 from .pipeline import PreprocessingPipeline
+from .normalization import NormalizationStrategy
