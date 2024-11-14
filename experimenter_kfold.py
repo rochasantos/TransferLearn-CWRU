638f0590cc66c929047d7c42c86363a13b354760
Adriano Rocha <adriano_angelo@live.com>
2024-11-14 20:23:16 -0300

    refactoring the cross validation experimenter.

--- experimenter_kfold.py
+++ experimenter_kfold.py
@@ -1,26 +1,47 @@
 from scripts.experiments.kfold import kfold
 from src.models.cnn2d import CNN2D
+from src.models.factory import ModelFactory
 
 import sys
 import logging
 from utils.logginout import LoggerWriter
+from utils.print_info import print_info
+
 
 def experimenter():
-    # Getting the model
-    model = CNN2D()
+    # Getting a model factory
+    model_factory = ModelFactory(CNN2D)
 
     # Training parameters 
-    num_epochs = 15
+    num_epochs = 5
     learning_rate = 0.001
     batch_size = 32
 
-    # Set folds
-    train_folds = ["fold2"]
-    test_folds = ["fold2"]
+    # Define the experiment setup.    
+    fold_split_sequence = [
+        {"train": (2, 3, 4), "test": (1,)},
+        {"train": (1, 3, 4), "test": (2,)},
+        {"train": (1, 2, 4), "test": (3,)},
+        {"train": (1, 2, 3), "test": (4,)},
+    ]
 
-    # Running the experiment    
-    kfold(model, train_folds, test_folds, num_epochs, learning_rate, batch_size)
+    # Log
+    experiment_title = "Sequential Cross-Validation Leave-P-Out"
+    print_info("Experiment", [experiment_title])
+    print_info("\nModel", [str(model_factory.create_model())])
+    print_info("\nTrain Parameters", [
+         f"num_epochs: {num_epochs}",
+         f"learning_rate: {learning_rate}",
+         f"batch_size: {batch_size}"])
+    print_info("\nSequence of setups for the experiment")
+    for sequence in fold_split_sequence:
+        print(f"{list(sequence.items())[0][0]}: {list(sequence.items())[0][1]} / {list(sequence.items())[1][0]}: {list(sequence.items())[1][1]}")
+    print("")
+
+    # Running the experiment 
+    kfold(model_factory, fold_split_sequence, num_epochs, learning_rate, batch_size)
+
 
 if __name__ == "__main__":
-    sys.stdout = LoggerWriter(logging.info)
+    sys.stdout = LoggerWriter(logging.info, "kfold")
     experimenter()
--- scripts/experiments/kfold.py
+++ scripts/experiments/kfold.py
@@ -1,61 +1,49 @@
-import numpy as np
 import torch
-import torch.nn as nn
-import torch.optim as optim
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
-from src.data_processing.dataset import SpectrogramImageDataset
-import copy
-import torch
-from torch.utils.data import DataLoader, Subset
-from sklearn.model_selection import StratifiedGroupKFold, StratifiedKFold
+from torch.utils.data import DataLoader, ConcatDataset
+import os
 from sklearn.metrics import confusion_matrix
-from scripts.experiments.helper import grouper
 from scripts.train_model import train_model
-from src.data_processing import DatasetManager
 
-def print_confusion_matrix(cm, class_names):
-    """Displays the confusion matrix in the console."""
-    print(f"{'':<3}" + "".join(f"{name:<5}" for name in class_names))
-    for i, row in enumerate(cm):
-        print(f"{class_names[i]:<3}" + "".join(f"{val:<5}" for val in row))
+def kfold(model_factory, fold_split_sequence, num_epochs=50, learning_rate=0.001, batch_size=32, device="cuda"):
 
-def kfold(model, train_folds, test_folds, num_epochs=50, learning_rate=0.001, batch_size=32, device="cuda"):
-
+    root_dir = "data/spectrograms/cwru_cv/"
+    
     transform = transforms.Compose([
         transforms.Resize((224)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406],  std=[0.229, 0.224, 0.225])
-    ]) 
+    ])
 
-    root_dir = "data/spectrograms/cwru_cv/"
-    
-    train_datasets = [
-        ImageFolder(root_dir + fold, transform) for fold in train_folds 
-    ]
-
-    test_datasets = [
-        ImageFolder(root_dir + fold, transform) for fold in test_folds   
-    ]    
-
-    initial_state = copy.deepcopy(model.state_dict())
-    
     total_accuracy = []
-    for train_dataset in train_datasets:                   
-        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+    for config in fold_split_sequence:
+        train_datasets = [ImageFolder(os.path.join(root_dir, f"fold{n_fold}"), transform) for n_fold in config["train"]]
+        train_concated_dataset = ConcatDataset(train_datasets)
+        # TRAINING
+        print('Starting model TRAINING...') 
+        print(f"Training folds: {list(config['train'])}")  
+        train_loader = DataLoader(train_concated_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
 
-        print('Starting model training...')   
-        model = train_model(model, train_loader, num_epochs, learning_rate, device, initial_state)
+        # Load the initial weights and biases
+        model = model_factory.create_model()
 
-        print('Training completed. Starting model evaluation...')
-        
-        for i, test_dataset in enumerate(test_datasets):
-            test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
+        # Training the model
+        model = train_model(model, train_loader, num_epochs, learning_rate, device)
+
+        # EVALUATION
+        print('\nStarting model EVALUATION...')      
+
+        for n_test_fold in config["test"]:
+            test_dataset_dir = os.path.join(root_dir, f"fold{n_test_fold}")
+            test_dataset = ImageFolder(test_dataset_dir, transform)                
+            test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+
             correct = 0
             total = 0
             fold_true_labels = []
             fold_predicted_labels = []
-
+           
             # Model evaluation loop
             with torch.no_grad():
                 for batch in test_loader:
@@ -72,12 +60,16 @@ def kfold(model, train_folds, test_folds, num_epochs=50, learning_rate=0.001, ba
 
             accuracy = 100 * correct / total
             total_accuracy.append(accuracy)
-            print(f'Model accuracy on the test set for Fold {i + 1}: {accuracy:.2f}%')
+            print(f'Model accuracy on the test set for fold {n_test_fold}: {accuracy:.2f}%')
 
             # Calculate and display the confusion matrix for the current fold, ensuring all classes are shown
-            cm = confusion_matrix(fold_true_labels, fold_predicted_labels)
-            print(f'Confusion Matrix for Fold {i + 1}:')
+            print(f'Confusion Matrix')
+            class_names = test_dataset.classes
+            class_indexes = list(range(len(class_names)))
+            cm = confusion_matrix(fold_true_labels, fold_predicted_labels, labels=class_indexes)
+            print(class_names)
             print(cm)
 
-    # total_accuracy_mean = sum(total_accuracy) / len(total_accuracy)
-    # print(f'Total Accuracy Mean: {total_accuracy_mean:.2f}%')
+    total_accuracy_mean = sum(total_accuracy) / len(total_accuracy)
+    print(f'\nTotal Accuracy Mean: {total_accuracy_mean:.2f}%')
+    print("")
