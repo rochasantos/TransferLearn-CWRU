d4355cb8f565301fefcf8b92bc381294250adc42
Adriano Rocha <adriano_angelo@live.com>
2024-11-14 21:52:26 -0300

    refactoring so that main is only responsible for preparing the experiment environment.

--- main.py
+++ main.py
@@ -1,33 +1,32 @@
+import sys
+import logging
+from utils.logginout import LoggerWriter
+
 from datasets import CWRU, UORED, Paderborn, Hust
-from scripts.create_spectrograms import create_spectrograms
-from src.preprocessing import PreprocessingPipeline, ResamplingStrategy, NormalizationStrategy
-from scripts.create_spectrograms import create_spectrograms
-from src.data_processing import DatasetManager
-from scripts.experiments.kfold import kfold
-from src.models import CNN2D, ResNet18
+from src.preprocessing import PreprocessingPipeline, ResamplingStrategy
 from scripts.download_rawfile import download_rawfile
+from scripts.spectrograms import create_cwru_spectrogram
+from scripts.copy_spectrogram_to_folds import copy_spectrogram_to_folds
 
 # SPECTROGRAMS
-def run_create_spectrograms():
-    target_sr = 48000
-    num_segments = 10
-    filter_config_path = 'config/filters_config.yaml'
+def create_spectrograms():
+    num_segments = 20
 
     # Creates the preprocessing pipeline and add the strategies to the pipeline
     preprocessing_pipeline = PreprocessingPipeline()
-    preprocessing_pipeline.add_step(ResamplingStrategy(target_sr=target_sr))
-    preprocessing_pipeline.add_step(NormalizationStrategy())
+    # preprocessing_pipeline.add_step(ResamplingStrategy())
 
     # Creation of spectrograms    
-    create_spectrograms(filter_config_path, preprocessing_pipeline, num_segments)                        
+    create_cwru_spectrogram(preprocessing_pipeline, num_segments) 
 
-# EXPERIMENTERS
-def run_experimenter():
-    model = ResNet18()    
-    kfold(model, group_by="extent_damage")
+# Copies the files to their corresponding folds.
+def copy_spectrograms_to_folds():                    
+    copy_spectrogram_to_folds()
 
 
 if __name__ == '__main__':
+    sys.stdout = LoggerWriter(logging.info)
     download_rawfile('CWRU')
-    run_create_spectrograms()
-    run_experimenter()
+    create_spectrograms()
+    copy_spectrograms_to_folds()
+    
